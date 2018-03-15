32e568a47c3d207ba5381a2df10832246770ed4f
Sacrimento <abouvero@e2r13p4.42.fr>
2018-03-15 15:58:27 +0100

    Visu added

--- Makefile
+++ Makefile
@@ -6,11 +6,12 @@
 #    By: abouvero <abouvero@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2018/03/10 17:01:23 by abouvero          #+#    #+#              #
-#    Updated: 2018/03/12 15:20:33 by abouvero         ###   ########.fr        #
+#    Updated: 2018/03/15 15:50:26 by abouvero         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 NAME = abouvero.filler
+NAME_VISU = visu
 LIBDIR = libft/
 LIBFT = ./libft/libft.a
 SRC = main.c \
@@ -20,7 +21,7 @@ SRC = main.c \
 	  free.c \
 
 OBJ = $(SRC:.c=.o)
-FLAGS = -Wall -Wextra -Werror -g
+FLAGS = -Wall -Wextra -Werror
 
 all: $(NAME)
 
@@ -38,7 +39,10 @@ clean:
 	@make -C $(LIBDIR) clean
 
 fclean: clean
-	@rm -f $(NAME)
+	@rm -f $(NAME) $(NAME_VISU)
 	@make -C $(LIBDIR) fclean
+
+visu: $(LIBFT) $(OBJ_VISU)
+	@gcc $(FLAGS) -o $(NAME_VISU) $(LIBFT) visualizer.c
 
 re: fclean all
--- visualizer.c
+++ visualizer.c
@@ -6,79 +6,94 @@
 /*   By: abouvero <abouvero@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/12 15:35:45 by abouvero          #+#    #+#             */
-/*   Updated: 2018/03/12 18:33:06 by abouvero         ###   ########.fr       */
+/*   Updated: 2018/03/15 15:56:45 by abouvero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "filler.h"
-#include <stdio.h>
 
-// void 	print_new_line
+void	print_colors(char *line, int *ret)
+{
+	int i;
 
-void 	print_grid(int fd)
+	i = 0;
+	while (line[4 + i])
+	{
+		if (line[4 + i] == '.')
+			ft_printf("\e[100m  \x1b[0m");
+		else if (line[4 + i] == 'O')
+			ft_printf("\x1b[48;5;126m  \x1b[0m");
+		else if (line[4 + i] == 'o')
+		{
+			ft_printf("\x1b[48;5;135m  \x1b[0m");
+			*ret = (*ret == 0 ? 1 : *ret);
+		}
+		else if (line[4 + i] == 'X')
+			ft_printf("\x1b[48;5;166m  \x1b[0m");
+		else if (line[4 + i] == 'x')
+		{
+			ft_printf("\x1b[48;5;172m  \x1b[0m");
+			*ret = (*ret == 0 ? 2 : *ret);
+		}
+		i++;
+	}
+}
+
+void	print_grid(int *score_p1, int *score_p2)
 {
 	char	*line;
-	int 	i;
+	int		ret;
 
 	line = NULL;
+	ret = 0;
 	while (get_next_line(0, &line))
 	{
-		if (ft_strstr(line, "Piece"))
+		if (ft_strstr(line, "Piece") || ft_strstr(line, "fin"))
 		{
+			if (ret == 1)
+				*score_p1 += 1;
+			else if (ret == 2)
+				*score_p2 += 1;
+			ft_printf("\n\n\x1b[38;5;126mPlayer 1 [%d]\x1b[0m\n\x1b[38;5;166m"
+							"Player 2 [%d]\x1b[0m\n\n", *score_p1, *score_p2);
 			ft_strdel(&line);
 			return ;
 		}
-		i = 0;
-		while (line[4 + i])
-		{
-			// ft_putendl_fd("BOUCLE",fd);
-			if (line[4 + i] == '.')
-				ft_printf("\e[100m \x1b[0m");
-			else if (line[4 + i] == 'O' || line[4 + i] == 'o')
-				ft_printf("\x1b[48;5;126m \x1b[0m");
-			else if (line[4 + i] == 'X' || line[4 + i] == 'x')
-				ft_printf("\x1b[48;5;166m \x1b[0m");
-			i++;
-		}
+		print_colors(line, &ret);
 		ft_printf("\n");
 		ft_strdel(&line);
 	}
 }
 
-void 	print_hundred_lines(void)
+void	print_hundred_lines(void)
 {
-	int 	i;
+	int		i;
 
 	i = 0;
-	while (i < 100)
+	while (i < 4)
 	{
-		write(1, "\n", 1);
+		write(1, "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n", 25);
 		i++;
 	}
 }
 
-int 	main(void)
+int		main(int argc, char **argv)
 {
 	char	*line;
+	int		score_p1;
+	int		score_p2;
 
 	line = NULL;
-	int fd = open("/dev/ttys001", O_RDWR);
-	//ft_printf("\e[100m "); // LIGHT GREY
+	score_p1 = 0;
+	score_p2 = 0;
 	while (get_next_line(0, &line))
 	{
-		if (ft_strstr(line, "=="))
-		{
-			//get_score();
-			break;
-		}
-		// if (ft_strstr(line, "Plateau"))
-		// 	print_hundred_lines();
-		if (ft_strstr(line, " "))
+		if (ft_isdigit(*line))
 		{
 			print_hundred_lines();
-			print_grid(fd);
+			print_grid(&score_p1, &score_p2);
 		}
-		usleep(50000);
+		usleep(argc == 2 && ft_atoi(argv[1]) > 0 ? ft_atoi(argv[1]) : 50000);
 		ft_strdel(&line);
 	}
 	return (0);
